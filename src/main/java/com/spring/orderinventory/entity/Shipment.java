91d4a5886298c894bd13a9dcae84d36c2e50966e
pavani <mpavanipavani06@gmail.com>
2024-11-14 22:18:54 +0530

    adding orderinventory

--- src/main/java/com/spring/orderinventory/entity/Shipment.java
+++ src/main/java/com/spring/orderinventory/entity/Shipment.java
@@ -1,7 +1,5 @@
 package com.spring.orderinventory.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
